b5b0be5d6218fbff2d371403ea1c630add16538a
gaoyongxiao <gaoyongxiao@meituan.com>
2018-09-11 00:41:56 +0800

    problem 27 -- remove element

--- LeetcodeWithSwift.playground/Contents.swift
+++ LeetcodeWithSwift.playground/Contents.swift
@@ -1,5 +1,26 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
-
-var str = "Hello, playground"
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var i = 0
+        var j = nums.count - 1
+        
+        while i <= j {
+            while i <= j && nums[i] != val {
+                i += 1
+            }
+            
+            while i <= j && nums[j] == val {
+                j -= 1
+            }
+            
+            if ( i <= j) {
+                nums[i] = nums[j]
+                i += 1
+                j -= 1
+            }
+        }
+        
+        return i
+    }
+}
